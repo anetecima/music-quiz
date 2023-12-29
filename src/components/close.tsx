ff48cd9df082b2f3bb218db05099ceece1ca7745
aosipovs <a.osipov@dyninno.lv>
2023-12-29 18:44:42 +0200

    features

--- src/components/close.tsx
+++
@@ -1,8 +0,0 @@
-import React, { SVGAttributes } from 'react'
-
-export const IconClose = (props: SVGAttributes<SVGElement>) => (
-    <svg width="1em" height="1em" viewBox="0 0 22.628 22.628" fill="#888a95" {...props}>
-        <rect width="30" height="2" rx="1" transform="rotate(45)" />
-        <rect width="30" height="2" rx="1" transform="translate(0 21.214) rotate(-45)" />
-    </svg>
-)
